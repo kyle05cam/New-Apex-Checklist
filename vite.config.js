2178fc2ab7ba9272e9c519944b0e9c8df98003b8
kyle05cam <kyle05cam@gmail.com>
2026-05-16 16:22:09 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: './',
   build: {
     outDir: 'dist',
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /.*\.jsx?$/,
+    exclude: [],
   }
 })
