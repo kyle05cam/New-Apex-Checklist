9eb8c9ffb3711a633714ef96154218752e04bbf9
kyle05cam <kyle05cam@gmail.com>
2026-05-16 17:10:06 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = 'apex-v1';
 const ASSETS = [
-  './',
-  './index.html',
-  './cessna172s_checklist.js',
-  './manifest.json',
+  '/',
+  '/index.html',
+  '/cessna172s_checklist.js',
+  '/manifest.json',
   'https://unpkg.com/react@18/umd/react.development.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.development.js',
   'https://unpkg.com/@babel/standalone/babel.min.js',
@@ -11,10 +11,26 @@ const ASSETS = [
   'https://unpkg.com/lucide@latest'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+// Installs the co-pilot (Service Worker) and downloads everything
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Flight Deck: Downloading assets for offline use...');
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting(); // Force it to activate immediately
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim()); // Take control of the app immediately
+});
+
+// The co-pilot intercepts requests and pulls from the local cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
